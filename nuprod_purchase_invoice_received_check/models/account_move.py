3e3bd29ead280d8363bcc72c56a0b8f25d19615b
nuprodRomainF <romain.frucco@nuprod.fr>
2026-05-11 11:30:11 +0200

    feat(nuprod_purchase_invoice_received_check): add computed fields, fully-received passes

--- nuprod_purchase_invoice_received_check/models/account_move.py
+++ nuprod_purchase_invoice_received_check/models/account_move.py
@@ -4,3 +4,36 @@ from odoo.exceptions import UserError
 
 class AccountMove(models.Model):
     _inherit = "account.move"
+
+    nu_has_invoice_over_received = fields.Boolean(
+        compute="_compute_nu_invoice_over_received",
+        store=False,
+    )
+    nu_invoice_over_received_warning = fields.Char(
+        compute="_compute_nu_invoice_over_received",
+        store=False,
+    )
+
+    def _nu_get_invoice_received_excesses(self):
+        """Return [(purchase_line, qty_invoiced_total, qty_received), ...]
+        for each purchase.order.line where the post-validation cumulative
+        invoiced quantity would exceed the received quantity."""
+        self.ensure_one()
+        return []
+
+    @api.depends(
+        "move_type", "state", "invoice_line_ids",
+        "invoice_line_ids.quantity",
+        "invoice_line_ids.purchase_line_id",
+        "invoice_line_ids.purchase_line_id.qty_received",
+        "invoice_line_ids.purchase_line_id.qty_invoiced",
+        "invoice_line_ids.product_id.type",
+    )
+    def _compute_nu_invoice_over_received(self):
+        for move in self:
+            excesses = (
+                move._nu_get_invoice_received_excesses()
+                if move.move_type == "in_invoice" else []
+            )
+            move.nu_has_invoice_over_received = bool(excesses)
+            move.nu_invoice_over_received_warning = False
--- nuprod_purchase_invoice_received_check/tests/test_invoice_received_check.py
+++ nuprod_purchase_invoice_received_check/tests/test_invoice_received_check.py
@@ -1,6 +1,57 @@
+from odoo.exceptions import UserError
 from odoo.tests.common import TransactionCase, tagged
 
 
 @tagged("post_install", "-at_install")
 class TestInvoiceReceivedCheck(TransactionCase):
-    """Placeholder, populated in subsequent tasks."""
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.partner = cls.env["res.partner"].create({"name": "Test Vendor"})
+        cls.product = cls.env["product.product"].create({
+            "name": "Test Storable",
+            "type": "consu",
+            "is_storable": True,
+            "purchase_method": "receive",
+        })
+        cls.service = cls.env["product.product"].create({
+            "name": "Test Service",
+            "type": "service",
+            "purchase_method": "receive",
+        })
+
+    def _make_po(self, product, qty, price=100.0):
+        po = self.env["purchase.order"].create({
+            "partner_id": self.partner.id,
+            "order_line": [(0, 0, {
+                "product_id": product.id,
+                "product_qty": qty,
+                "price_unit": price,
+            })],
+        })
+        po.button_confirm()
+        return po
+
+    def _receive(self, po, qty):
+        """Validate the first picking with the given received quantity."""
+        picking = po.picking_ids.filtered(lambda p: p.state not in ("done", "cancel"))[:1]
+        if not picking:
+            return
+        move = picking.move_ids[:1]
+        move.write({"quantity": qty, "picked": True})
+        picking._action_done()
+
+    def _make_invoice_from_po(self, po):
+        """Create a draft bill from the PO (Odoo prefills lines from PO)."""
+        po.action_create_invoice()
+        return po.invoice_ids[-1]
+
+    def test_invoice_fully_received(self):
+        po = self._make_po(self.product, qty=10)
+        self._receive(po, qty=10)
+        invoice = self._make_invoice_from_po(po)
+        invoice.invoice_line_ids.write({"quantity": 10})
+        invoice.action_post()
+        self.assertEqual(invoice.state, "posted")
+        self.assertFalse(invoice.nu_has_invoice_over_received)
