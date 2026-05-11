9e9b37355e5927617bf8f6cdb51d7104d1321d14
nuprodRomainF <romain.frucco@nuprod.fr>
2026-05-11 11:29:42 +0200

    feat(nuprod_purchase_invoice_received_check): module skeleton

---
+++ nuprod_purchase_invoice_received_check/tests/test_invoice_received_check.py
@@ -0,0 +1,6 @@
+from odoo.tests.common import TransactionCase, tagged
+
+
+@tagged("post_install", "-at_install")
+class TestInvoiceReceivedCheck(TransactionCase):
+    """Placeholder, populated in subsequent tasks."""
