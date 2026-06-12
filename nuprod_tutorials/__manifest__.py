ef2e3a6bd70cf168015b8c53c064ece5405592af
nuprodRomainF <romain.frucco@nuprod.fr>
2026-06-12 09:45:15 +0200

    New module tutorial 	new file:   nuprod_tutorials/__init__.py 	new file:   nuprod_tutorials/__manifest__.py 	new file:   nuprod_tutorials/models/__init__.py 	new file:   nuprod_tutorials/models/models.py 	new file:   nuprod_tutorials/static/description/icon.png 	new file:   nuprod_tutorials/views/nuprod_redirect.xml

---
+++ nuprod_tutorials/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Nuprod Tutorials',
+    'version': '19.0.1.0.0',
+    'summary': 'A module to redirect to the darnajou tutorials',
+    'description': 'A module to redirect to the darnajou tutorials',
+    'author': 'Nuprod',
+    'website': 'https://www.nuprod.fr',
+    'category': 'Tools',
+    'depends': ['base'],
+    'data': [
+        'views/nuprod_redirect.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
